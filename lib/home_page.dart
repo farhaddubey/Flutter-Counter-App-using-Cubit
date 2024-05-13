0260d0f5ec56d5c447db608a58529c01f5e10434
Farhad Dubey <farhaddubey003@gmail.com>
2024-05-13 16:53:35 +0530

    shifting_to_flutter_BlocBuilder

---
+++ lib/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:counter_app/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final counterCubit = CounterCubit();
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = counterCubit.state;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // onPressed: ()=>counterCubit.increment(),
+        onPressed: (){
+          counterCubit.increment();
+          setState(() {
+
+          });
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
